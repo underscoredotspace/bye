cff16a4a4477bac4db33f5fbe76825e0393d2beb
Colin Tindle <ampersand@underscore.space>
2019-03-21 15:03:08 +0000

    rename to countdown add new countdown handle event passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// const targetDate = new Date('Thu Mar 28 2018 16:00:00 GMT+0100') // RBS
+const targetDate = new Date('Tue Apr 16 2019 10:00:00 GMT+0100') //Fanduel
+const eventTitle = "Starting at Fanduel!"
+
 function timeUntil(date) {
   const now = new Date()
   const diff = dateDiff(now, date)
@@ -5,21 +9,27 @@ function timeUntil(date) {
   const h = Math.floor((diff - (d * 60 * 60 * 24)) / (60 * 60))
   const m = Math.floor((diff - (d * 60 * 60 * 24) - (h * 60 * 60)) / 60)
   const s = Math.floor((diff - (d * 60 * 60 * 24) - (h * 60 * 60) - (m * 60)))
-  return {d, h, m, s}
+  return { d, h, m, s }
 }
 
 function dateDiff(a, b) {
   const diffMS = b.getTime() - a.getTime()
-  return Math.floor(diffMS / 1000)
+  return Math.floor(diffMS / 1000) // difference in seconds rounded down to the nearest one
 }
 
 function updateCountdown() {
+  if (targetDate <= new Date()) {
+    document.querySelector('#due').classList.add('hidden')
+    document.querySelector('#done').classList.remove('hidden')
+    return
+  }
+
   requestAnimationFrame(updateCountdown)
   const $days = document.querySelector('#countdown #days .value')
   const $hours = document.querySelector('#countdown #hours .value')
   const $minutes = document.querySelector('#countdown #minutes .value')
   const $seconds = document.querySelector('#countdown #seconds .value')
-  const {d, h, m, s} = timeUntil(targetDate)
+  const { d, h, m, s } = timeUntil(targetDate)
 
   if (Number($days.innerText) !== d) $days.innerText = d
   if (Number($hours.innerText) !== h) $hours.innerText = h
@@ -27,8 +37,12 @@ function updateCountdown() {
   if (Number($seconds.innerText) !== s) $seconds.innerText = s
 }
 
-const targetDate = new Date('Thu Mar 28 2018 16:00:00 GMT+0100')
-const $eventDate = document.querySelector('#date')
-$eventDate.innerText = targetDate.toString().substr(0,21)
+(() => {
+  const $eventDate = document.querySelector('#date')
+  $eventDate.textContent = targetDate.toString().substr(0, 21)
 
-updateCountdown()
+  const $eventTitle = document.querySelector('#event')
+  $eventTitle.textContent = eventTitle
+
+  updateCountdown()
+})()
